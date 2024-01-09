74478905c552fb3545c7579328d0626bc7fdafb4
Jordan Pratt <jpratt@mtroyal.ca>
2024-01-09 06:55:28 -0700

    Add movies.

---
+++ movies.php
@@ -0,0 +1,18 @@
+$movies = [
+    [
+        "id" => "10775",
+        "title" => "Infernal Affairs"
+    ],
+    [
+        "id" => "508883",
+        "title" => "The Boy and the Heron"
+    ],
+    [
+        "id" => "940721",
+        "title" => "Godzilla Minus One"
+    ],
+    [
+        "id" => "365620",
+        "title" => "Ferrari"
+    ],
+];
