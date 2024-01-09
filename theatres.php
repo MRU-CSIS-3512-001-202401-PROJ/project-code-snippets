f2c84b69f508d5cc6b7831b1364744a353c3e538
Jordan Pratt <jpratt@mtroyal.ca>
2024-01-09 06:17:44 -0700

    Add theatres.

---
+++ theatres.php
@@ -0,0 +1,27 @@
+<?php
+$theatres = [
+    [
+        "name" => "Plaza Theatre",
+        "address" => "1133 Kensington Rd NW",
+        "lat" => 51.052412578151916,
+        "long" => -114.08788680334811
+    ],
+    [
+        "name" => "Globe Cinema",
+        "address" => "617 8 Ave SW",
+        "lat" => 51.045686640675555,
+        "long" => -114.07502973218419
+    ],
+    [
+        "name" => "Scotiabank Theatre Chinook",
+        "address" => "6455 Macleod Trail SW",
+        "lat" => 50.99660636689441,
+        "long" => -114.07416176102294
+    ],
+    [
+        "name" => "Landmark Cinemas 5 Market Mall",
+        "address" => "3412 49 St NW",
+        "lat" => 51.08298029486308,
+        "long" => -114.15741816357233
+    ],
+];
