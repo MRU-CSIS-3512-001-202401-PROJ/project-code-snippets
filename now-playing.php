f35c8399629f27055c5b7e8181edc02b3028d89f
Jordan Pratt <jpratt@mtroyal.ca>
2024-01-09 13:17:52 -0700

    Add now-playing data.

---
+++ now-playing.php
@@ -0,0 +1,37 @@
+$now_playing = [
+    [
+        "id" => 1,
+        "theatre_id" => 1,
+        "movie_id" => "10775"
+    ],
+    [
+        "id" => 2,
+        "theatre_id" => 1,
+        "movie_id" => "508883"
+    ],
+    [
+        "id" => 3,
+        "theatre_id" => 1,
+        "movie_id" => "940721"
+    ],
+    [
+        "id" => 4,
+        "theatre_id" => 2,
+        "movie_id" => "840430"
+    ],
+    [
+        "id" => 5,
+        "theatre_id" => 3,
+        "movie_id" => "508883"
+    ],
+    [
+        "id" => 6,
+        "theatre_id" => 3,
+        "movie_id" => "365620"
+    ],
+    [
+        "id" => 7,
+        "theatre_id" => 4,
+        "movie_id" => "365620"
+    ]
+];
