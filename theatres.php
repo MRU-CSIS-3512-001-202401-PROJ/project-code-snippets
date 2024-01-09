d7dd7c10d41c1822f6da99b08014218271006a06
Jordan Pratt <jpratt@mtroyal.ca>
2024-01-09 13:03:47 -0700

    Added ids to theatres.

--- theatres.php
+++ theatres.php
@@ -1,23 +1,27 @@
 $theatres = [
     [
+        "id" => 1,
         "name" => "Plaza Theatre",
         "address" => "1133 Kensington Rd NW",
         "lat" => 51.052412578151916,
         "long" => -114.08788680334811
     ],
     [
+        "id" => 2,
         "name" => "Globe Cinema",
         "address" => "617 8 Ave SW",
         "lat" => 51.045686640675555,
         "long" => -114.07502973218419
     ],
     [
+        "id" => 3,
         "name" => "Scotiabank Theatre Chinook",
         "address" => "6455 Macleod Trail SW",
         "lat" => 50.99660636689441,
         "long" => -114.07416176102294
     ],
     [
+        "id" => 4,
         "name" => "Landmark Cinemas 5 Market Mall",
         "address" => "3412 49 St NW",
         "lat" => 51.08298029486308,
